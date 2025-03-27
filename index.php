065f8441c27be0263d39e1f868bafe2f41742c00
Ren4t4 <129637393+Ren4t4@users.noreply.github.com>
2025-03-27 08:55:39 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 20%;
+        }
+    </style>
+</head>
+<body>
+    <h1><?php echo "Hola Mundo"; ?></h1>
+</body>
+</html>
